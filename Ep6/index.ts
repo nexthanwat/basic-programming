0d1ae5d2b8b12f24db6459861518758c60c7152a
nexthanwat <nexpair@gmail.com>
2024-08-30 10:39:45 +0700

    Array part 2

---
+++ Ep6/index.ts
@@ -0,0 +1,34 @@
+// const score:number[] = [10,23,26,30,33]
+
+// let sum:number = 0
+// for (let i = 0; i < score.length; i++) {
+//     sum = sum + score[i] หาผลรวม
+// }
+// const avg = sum / score.length หาค่าเฉลี่ย
+// console.log(sum)
+// console.log(avg)
+
+// const sName: string = 'n1, n2, n3, n4, n5, n6'
+
+// const sNameArray = sName.split(',')
+// console.log(sNameArray.join('#')) 
+
+// const Aname: string = "หมา*แมว*ช้าง*เสือ"
+
+// const AnameArray: string[] = Aname.split('*')
+// console.log(AnameArray)
+
+// const Aname: string = "หมา*แมว*ช้าง*เสือ"
+
+// const AnameArray: string[] = Aname.split('*')
+// AnameArray.pop()// ดึงตัวสุดท้ายออก
+// AnameArray.push('กระต่าย')//ใส่คำท้ายใหม่
+// AnameArray.shift()// ดึงตัวหน้าออก
+// AnameArray.unshift('ยีราฟ')// ใส่ของเข้าไปในตัวหน้า
+// //console.log(AnameArray.sort())//เรียงลำดับขั้น a-z ก-ฮ
+// //console.log(AnameArray.reverse())//เรียงกลับ z-a ฮ-ก
+// console.log(AnameArray.slice(0,2))// เอาคำออกมาตามลำดับที่กำหนด
+
+const fruits = ["Banana","Orange","Apple","Mango"]
+console.log(fruits.sort())
+console.log(fruits.reverse())
